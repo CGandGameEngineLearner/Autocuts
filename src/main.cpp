775d9d53130f07cb515776dfe2a06f1068a44311
CGandGameEngineLearner <lifesize1@qq.com>
2024-12-02 15:14:26 +0800

    refactor

---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+#include "ApplicationPipeline.h"
+
+#include <omp.h>
+#include <igl/viewer/Viewer.h>
+
+using namespace std;
+using namespace igl::viewer;
+
+int main(int argc, char** argv)
+{
+	char* num_threads_str = getenv("OMP_NUM_THREADS");
+	unsigned int num_threads = 1;
+
+	if (num_threads_str != nullptr)
+	{
+		num_threads = max((unsigned int)atoi(getenv("OMP_NUM_THREADS")), num_threads);
+	}
+
+	omp_set_num_threads(num_threads);
+
+	Viewer viewer;
+	viewer.callback_init = [&](Viewer& v) {
+		// parse command line arguments
+		bool is_loaded = false;
+		for (int i = 1; i < argc; ++i)
+		{
+			if (strcmp(argv[i], "-s") == 0)
+			{
+				cout << "load scene file: " << argv[i + 1] << endl;
+				is_loaded = v.load_scene(argv[i + 1]);
+				if (is_loaded == false)
+				{
+					cout << "file not found: " << argv[i + 1] << endl;
+				}
+			}
+		}
+		return false;
+	};
+
+	// add pipieline plugin
+	ApplicationPipeline pipeline;
+	viewer.core.rotation_type = igl::viewer::ViewerCore::ROTATION_TYPE_TRACKBALL;
+	viewer.plugins.push_back(&pipeline);
+	viewer.core.background_color << 1., 1., 1., 1.; // 0.25, 0.25, 0.25, 1.0;
+	pipeline.SetArguments(argc, argv);
+
+	// set to ortographic view to synchronize pointer with models
+	viewer.core.orthographic = true;
+
+	// start viewer
+	viewer.launch(true, false, 2400, 1350);
+
+	return 0;
+}
